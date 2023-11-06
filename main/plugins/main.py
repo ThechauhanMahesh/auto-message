c23aa3b4ad611d0b4ab33e6f3a1fadf4cf484630
ThechauhanMahesh <125787905+ThechauhanMahesh@users.noreply.github.com>
2023-11-06 10:56:34 +0530

    Update main.py

--- main/plugins/main.py
+++ main/plugins/main.py
@@ -3,12 +3,12 @@
 from .. import bot
 from telethon import events
 
-text = """Heya👋, Thanks for joining our channel. (This is an automated message)
+text = """lThanks for joining our channel. (This is an automated message)
 
-Do check out our other bot : (Click here)[https://t.me/Useful_Premium_Bots/2]"""
+**Do check out our other bot :** (Click here)[https://t.me/Useful_Premium_Bots/2]"""
 
 @bot.on(events.ChatAction)
 async def _(event):
     if event.user_joined or event.user_added:
         user = await event.get_user()
-        await bot.send_message(user.id, f"Hey {user.first_name}\n\n{text}")
+        await bot.send_message(user.id, f"Heya👋, {user.first_name}\n\n{text}")
